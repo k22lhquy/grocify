a59cd672197d8c1b1ef5f3a5ecfd2afdacaf4a9f
Quy le <quyle150802@gmail.com>
2026-03-18 01:22:20 +0700

    login

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,10 @@
-import { ClerkProvider } from '@clerk/expo'
-import { tokenCache } from '@clerk/expo/token-cache'
-import { Slot } from 'expo-router'
-import { Stack } from 'expo-router'
+import '../../global.css';
+
+import { ClerkProvider } from '@clerk/expo';
+import { tokenCache } from '@clerk/expo/token-cache';
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { Stack } from 'expo-router';
+import { useColorScheme } from 'react-native';
 
 const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
 
@@ -10,9 +13,12 @@ if (!publishableKey) {
 }
 
 export default function RootLayout() {
+  const colorScheme = useColorScheme()
   return (
     <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
-      <Stack screenOptions={{headerShown: false}}/>
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <Stack screenOptions={{ headerShown: false }} />
+      </ThemeProvider>
     </ClerkProvider>
   )
 }
